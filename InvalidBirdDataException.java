73222cc248fa30944290e00eda182c340d809502
Alejandro Cueto <s3813718@students.fscj.edu>
2026-07-29 14:55:34 -0400

    Update InvalidBirdDataException.java

--- InvalidBirdDataException.java
+++ InvalidBirdDataException.java
@@ -2,3 +2,12 @@
 // Alejandro Cueto
 // 7/28/26
 // Invalid Bird Data
+
+
+package edu.fscj.cop2800c.penguin;
+
+public class InvalidBirdDataException extends Exception {
+    public InvalidBirdDataException(String message) {
+        super(message);
+    }
+}
