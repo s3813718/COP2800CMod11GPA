aee7cc0430410cc2572f8ffdedbaac081e9ebd79
Alejandro Cueto <s3813718@students.fscj.edu>
2026-07-29 14:57:49 -0400

    Update Penguin.java

--- edu/fscj/cop2800c/penguin/Penguin.java
+++ edu/fscj/cop2800c/penguin/Penguin.java
@@ -1,21 +1,36 @@
 // Penguin.java
-// D. Singletary
-// 3/15/25
+// Alejandro Cueto
+// 7/29/26
 // Represents a Palmer Penguin
 
 package edu.fscj.cop2800c.penguin;
 
 // Penguin class extends the Bird base class
 public class Penguin extends Bird {
+
     // private fields
     private String species;
     private double flipperLength;
 
     // Constructor
-    public Penguin(int sampleNum, String species, 
-                   double culmenLength, double culmenDepth, 
-                   double bodyMass, String sex, double flipperLength) {
+    public Penguin(int sampleNum, String species,
+                   double culmenLength,
+                   double culmenDepth,
+                   double bodyMass,
+                   String sex,
+                   double flipperLength)
+                   throws InvalidBirdDataException {
+
         super(sampleNum, culmenLength, culmenDepth, bodyMass, sex);
+
+        if (flipperLength < 0 ||
+            species == null ||
+            species.isEmpty()) {
+
+            throw new InvalidBirdDataException(
+                    "Invalid Penguin data encountered.");
+        }
+
         this.species = species;
         this.flipperLength = flipperLength;
     }
@@ -29,35 +44,44 @@ public class Penguin extends Bird {
         return flipperLength;
     }
 
-    // Override Bird class's toString method
+    // Override toString
     @Override
     public String toString() {
-        return super.toString() + 
+        return super.toString() +
                ", species='" + species + '\'' +
                ", flipperLength=" + flipperLength;
     }
 
-    // Override Bird's compareTo to include Penguin field comparison
+    // Override compareTo
     @Override
     public int compareTo(Bird other) {
+
         int result = super.compareTo(other);
         if (result != 0) return result;
 
-        // Check if 'other' is a Penguin before 
-        // comparing flipper length and species
         if (other instanceof Penguin) {
+
             Penguin otherPenguin = (Penguin) other;
-    
-            // Compare flipper length
+
             result = Double.compare(
-                this.flipperLength, otherPenguin.flipperLength);
+                    this.flipperLength,
+                    otherPenguin.flipperLength);
+
             if (result != 0) return result;
-    
-            // Compare species (handle potential null values)
-            if (this.species == null && otherPenguin.species == null) return 0;
-            if (this.species == null) return -1;
-            if (otherPenguin.species == null) return 1;
-    
+
+            if (this.species == null &&
+                otherPenguin.species == null) {
+                return 0;
+            }
+
+            if (this.species == null) {
+                return -1;
+            }
+
+            if (otherPenguin.species == null) {
+                return 1;
+            }
+
             return this.species.compareTo(otherPenguin.species);
         }
 
